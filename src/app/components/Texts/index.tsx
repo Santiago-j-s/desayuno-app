279512660ff9c86c10489446c56780644e59efad
Santiago Santana <santana.santiago@gmail.com>
2025-04-27 16:54:33 -0300

    refactor: create TextData to show inside Suspense

--- src/app/components/Texts/index.tsx
+++ src/app/components/Texts/index.tsx
@@ -2,36 +2,27 @@ import { getTextsFromSheet } from "@/services/sheets";
 import { TextLineSkeleton, TextsClient } from "./Texts.client";
 import { auth } from "@/services/auth";
 import { Suspense } from "react";
+import { unstable_ViewTransition as ViewTransition } from "react";
 
-async function getTexts() {
+async function TextsData() {
   const session = await auth();
 
   if (!session) {
-    return [];
+    return (
+      <div className="text-center text-sm text-red-500">Error: No session</div>
+    );
   }
 
-  return getTextsFromSheet(session.access_token);
+  const texts = await getTextsFromSheet(session.access_token);
+
+  return <TextsClient texts={texts} />;
 }
 
 export async function Texts() {
-  const textsData = getTexts();
-
   return (
     <div className="flex w-full flex-col gap-4 rounded-xl border border-gray-800 bg-gray-900 p-6 shadow-lg">
-      <Suspense
-        fallback={
-          <>
-            {Array.from({ length: 3 }).map((_, index) => (
-              <TextLineSkeleton key={index} />
-            ))}
-          </>
-        }
-      >
-        {textsData
-          .then((texts) => <TextsClient texts={texts} />)
-          .catch((error) => (
-            <div>Error: {error.message}</div>
-          ))}
+      <Suspense>
+        <TextsData />
       </Suspense>
     </div>
   );
